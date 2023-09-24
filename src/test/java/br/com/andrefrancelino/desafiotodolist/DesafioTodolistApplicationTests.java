9358c4f4017ffc3e40d1eba6902f4e8971f7f118
Andre Francelino <andrefgomes.github.version@gmail.com>
2023-09-24 11:14:34 -0300

    Testes Automatizados

--- src/test/java/br/com/andrefrancelino/desafiotodolist/DesafioTodolistApplicationTests.java
+++ src/test/java/br/com/andrefrancelino/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -1,13 +1,48 @@
 package br.com.andrefrancelino.desafiotodolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.andrefrancelino.desafiotodolist.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DesafioTodolistApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testCreateTodoSucess() {
+		var todo = new Todo("Boxe", "Ir treinar um pouco de boxe na academia.", false, 1);
+
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+
+	}
+
+	@Test
+	void testCreateTodoFailure() {
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(
+				new Todo("", "", false, 0)
+			).exchange()
+			.expectStatus().isBadRequest();
 	}
 
 }
